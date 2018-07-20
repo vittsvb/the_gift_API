9e71199d6c3937dee91604af8b00a7b49fcf250c
felipecisotto <felipecisotto@hotmail.com>
2018-07-19 22:06:57 -0300

    deletar e comeco do inserir

--- controller/givenPersonController.js
+++ controller/givenPersonController.js
@@ -4,51 +4,67 @@ var givenPerson = require('../models/givenPerson')
 let registerGivenPerson = function (userid, type, age, profession, sex, hobbie, presentValue, occasion) {
 
     return new Promise(async function (resolve, reject) {
-        
+
         try {
-           
+
             console.log('entrou');
             //Cria o novo presenteado
             var newGivenPerson = new givenPerson({
-                type : type,
-                age : age,
-                profession : profession,
-                sex : sex,
-                hobbie : hobbie,
-                presentValue : presentValue,
-                occasion : occasion
+                type: type,
+                age: age,
+                profession: profession,
+                sex: sex,
+                hobbie: hobbie,
+                presentValue: presentValue,
+                occasion: occasion
             })
-            
+
             newGivenPerson = await newGivenPerson.save()
 
-            if (newGivenPerson){
-                user.findByIdAndUpdate(userid,{$set: { givenPerson: newGivenPerson._id }})
+            if (newGivenPerson) {
+                user.findByIdAndUpdate(userid, { $set: { givenPerson: newGivenPerson._id } })
             }
-            
-            return resolve('Salvo com sucesso',true)
+
+            return resolve('Salvo com sucesso', true)
 
         } catch (err) {
-            
+
             return reject('Erro ao registrar presenteado', err)
-            
+
         }
     })
 
 }
 
-let deleteGivenPerson = function (givenPersonid){
+let deleteGivenPerson = function (givenPersonid) {
+    return new Promise(async function (resolve, reject) {
+        try {
 
-    try{
+            return resolve(givenPerson.findByIdAndRemove(givenPersonid))
 
-        return resolve(givenPerson.findByIdAndRemove(givenPersonid))
-
-    }
-    catch(err){
-        return reject('Erro ao deletar presenteado', err)
-    }
-
+        }
+        catch (err) {
+            console.log(err);
+            return reject('Erro ao deletar presenteado', err)
+        }
+    })
 
 }
+let findallGivenPerson = function (givenPersonid) {
+    return new Promise(async function (resolve, reject) {
+        try {
+
+            return resolve(givenPerson.findByIdAndRemove(givenPersonid))
+
+        }
+        catch (err) {
+            console.log(err);
+            return reject('Erro ao deletar presenteado', err)
+        }
+    })
+
+}
+
 
 
 module.exports = {
